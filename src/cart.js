23bfd93810bc825fd27d473052d0a703e7d6aeb0
HesamWebDesign <hesamwebdesigner@gmail.com>
2023-05-13 11:47:16 -0800

    Make cart.js Responsive

--- src/cart.js
+++ src/cart.js
@@ -13,15 +13,24 @@ let shoppingCart = document.getElementById("shopping-cart");
 
 let generateCartItems = () => {
   if (basket.length !== 0) {
-    console.log("basket full");
+    // shoppingCart.innerHTML = basket.map((x) => {
+    //   let { id, item } = x;
+    //   let search = shopItemData.find((y) => y.id === id) || [];
+    //   console.log(search);
+    //   return `
+    //   <div class="cart-item">
+    //   <img src=${search.img} alt=${search.name} />
+    //   </div>
+    //   `;
+    // });
   } else {
-    label.innerHTML = `
-    <h2>Cart is Empty</h2>
-    <a href="/index.html">
-        <button class="homeBtn">Back to Home</button>
-    </a>
-`;
-    shoppingCart.innerHTML = ``;
+    //     label.innerHTML = `
+    //     <h2>Cart is Empty</h2>
+    //     <a href="/index.html">
+    //         <button class="homeBtn">Back to Home</button>
+    //     </a>
+    // `;
+    //     shoppingCart.innerHTML = ``;
   }
 };
 generateCartItems();
