ac80b46ead9db1eda97ef2dabdc4492c809130b1
HesamWebDesign <hesamwebdesigner@gmail.com>
2023-05-15 01:13:01 +0330

    Finish the Shopping-Cart project
    It can be much better but let's say "finish" for now

--- src/cart.js
+++ src/cart.js
@@ -13,38 +13,30 @@ let shoppingCart = document.getElementById("shopping-cart");
 
 let generateCartItems = () => {
   if (basket.length !== 0) {
-    label.innerHTML = `
-        <h2>Total Bill: $1000</h2>
-        <a class="checkoutLink" href="#">
-          <button class="checkout">Checkout</button>
-        </a>
-        <a class="clearCartLink" href="#">
-          <button class="clearCart">Clear Cart</button>
-        </a>
-    `;
     shoppingCart.innerHTML = basket
       .map((x) => {
         let { id, item } = x;
         let search = shopItemData.find((y) => y.id === id) || [];
-        console.log(search);
         return `
       <div class="cart-item">
+          <div onclick="removeItem(${id})" class="close-icon">
+            <img src="/icons/close.svg" alt="Close" />
+          </div>
           <img src=${search.img} alt=${search.name} />
           <div class="details">
             <h3 class="title">${search.name}</h3>
-            <p class="description">
-            ${search.desc}            </p>
+            <p class="description">${search.desc}</p>
             <div class="price-quantity">
               <h2>$ ${search.price}</h2>
               <div class="quantity-button">
-                <img src="/icons/Minus.png" alt="Minus" />
-                <div class="quantity">0</div>
-                <img src="/icons/Plus.png" alt="Plus" />
-              </div>
+              <img onclick="decrement(${id})" src="/icons/Minus.png" alt="Minus" />
+              <div id=${id} class="quantity">${item}</div>
+              <img onclick="increment(${id})" src="/icons/Plus.png" alt="Plus" />
+            </div>
             </div>
           </div>
-          <div class="close-icon">
-            <img src="/icons/close.svg" alt="Close" />
+          <div id=total-price-${id} class="total-price">
+          Total price: $${item * search.price}
           </div>
         </div>
       `;
@@ -61,3 +53,83 @@ let generateCartItems = () => {
   }
 };
 generateCartItems();
+
+let increment = (id) => {
+  let selectedItem = id;
+  console.log(selectedItem);
+  let search = basket.find((x) => x.id === selectedItem.id);
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+  // ! It should solve (generate the whole page with every increment is awful!)
+  generateCartItems();
+  updateQuantity(selectedItem.id);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let decrement = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+  if (search === undefined || search.item === 0) {
+    return;
+  } else {
+    search.item -= 1;
+  }
+  // ! It should solve (generate the whole page with every decrement is awful!)
+  generateCartItems();
+  updateQuantity(selectedItem.id);
+  if (search.item === 0) {
+    basket = basket.filter((x) => x.item !== 0);
+    generateCartItems();
+  }
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let updateQuantity = (id) => {
+  let search = basket.find((x) => x.id === id);
+  document.getElementById(id).innerHTML = search.item;
+  cartAmountCalc();
+  totalAmount();
+};
+
+let removeItem = (id) => {
+  let selectedItem = id;
+  basket = basket.filter((x) => x.id !== selectedItem.id);
+  generateCartItems();
+  totalAmount();
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let clearCart = () => {
+  basket = [];
+  generateCartItems();
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let totalAmount = () => {
+  console.log("in totalAmount");
+  if (basket.length !== 0) {
+    let amount = basket
+      .map((x) => {
+        let { item, id } = x;
+        let search = shopItemData.find((y) => y.id === id) || [];
+        return item * search.price;
+      })
+      .reduce((x, y) => x + y, 0);
+    label.innerHTML = `
+      <h2>Total Bill: $${amount}</h2>
+      <a class="checkoutLink" href="#">
+        <button class="checkout">Checkout</button>
+      </a>
+      <a class="clearCartLink" href="#">
+        <button onclick="clearCart()" class="clearCart">Clear Cart</button>
+      </a>
+    `;
+  } else return;
+};
+totalAmount();
